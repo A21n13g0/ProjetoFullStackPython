eb5ebf58b77c78ed012cfac5e9737a5b7d128289
A21n13g0 <angelo.nascimentojr@outlook.com>
2023-04-24 21:44:03 -0300

    Primero envio

--- projectpythonfullstack/settings.py
+++ projectpythonfullstack/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 import django_heroku
 
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-(tvimq3s!_pxfi_ouf2z^2^a84-nxidll!^3#)p-u1re=v4r1y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,7 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-django_heroku.settings(locals())
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
